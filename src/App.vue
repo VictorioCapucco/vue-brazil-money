e12953ac39a843e616595f75fc8bfa2d3c7f175d
Victório Capucço <victorio.capucco.oliveira@gmail.com>
2022-12-21 22:06:39 -0300

    update tests

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Returned value {{ test }}</p>
     Formatted value:
-    <money v-model="test" :decimalPlaces="5" class="text-red" />
+    <money v-model="test" :decimalPlaces="5" id="input" class="text-red" />
   </div>
 </template>
 
